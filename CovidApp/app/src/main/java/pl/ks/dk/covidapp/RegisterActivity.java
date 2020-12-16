d2db8b0b8868c55fa09a89f1b5cdc1d069a39b1e
Krzysztof Szczesniak <krzys.szcze@gmail.com>
2020-12-16 16:51:59 +0100

    new fields to registration activity added

--- CovidApp/app/src/main/java/pl/ks/dk/covidapp/RegisterActivity.java
+++ CovidApp/app/src/main/java/pl/ks/dk/covidapp/RegisterActivity.java
@@ -4,11 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,12 +23,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.stream.Stream;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    MaterialEditText username, email, password;
+    MaterialEditText name, surname, email, password, pesel, phoneNumber, dateOfBirth;
     Button btn_register;
+    DatePickerDialog pickerDialog;
 
     FirebaseAuth auth;
     DatabaseReference reference;
@@ -40,32 +48,64 @@ public class RegisterActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Register");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        username = findViewById(R.id.username);
+        name = findViewById(R.id.name);
+        surname = findViewById(R.id.surname);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
+        pesel = findViewById(R.id.pesel);
+        phoneNumber = findViewById(R.id.phone_number);
+        dateOfBirth = findViewById(R.id.birth_date);
         btn_register = findViewById(R.id.btn_register);
 
         auth = FirebaseAuth.getInstance();
 
+        dateOfBirth.setInputType(InputType.TYPE_NULL);
+        dateOfBirth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                pickerDialog = new DatePickerDialog(RegisterActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        String date = dayOfMonth + "/" + (month + 1) + "/" + year;
+                        dateOfBirth.setText(date);
+                    }
+                }, year, month, day);
+                pickerDialog.show();
+            }
+        });
+
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String txt_username = username.getText().toString();
+                String txt_name = name.getText().toString();
+                String txt_surname = surname.getText().toString();
+                String txt_username = txt_name.concat(txt_surname);
                 String txt_email = email.getText().toString();
                 String txt_password = password.getText().toString();
+                String txt_pesel = pesel.getText().toString();
+                String txt_phone_number = phoneNumber.getText().toString();
+                String txt_date_of_birth = dateOfBirth.getText().toString();
 
-                if (TextUtils.isEmpty(txt_username) || TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
+                boolean valid =  Stream.of(txt_name, txt_surname, txt_username, txt_email, txt_password, txt_pesel, txt_phone_number, txt_date_of_birth)
+                        .allMatch(StringUtils::isNotBlank);
+
+                if (!valid) {
                     Toast.makeText(RegisterActivity.this, R.string.required_fields, Toast.LENGTH_SHORT).show();
                 } else if (txt_password.length() < 8) {
                     Toast.makeText(RegisterActivity.this, R.string.required_length_password, Toast.LENGTH_SHORT).show();
                 } else {
-                    register(txt_username, txt_email, txt_password);
+                    register(txt_username, txt_email, txt_password, txt_pesel, txt_phone_number, txt_date_of_birth);
                 }
             }
         });
     }
 
-    private void register(String username, String email, String password) {
+    private void register(String username, String email, String password, String pesel, String phoneNumber, String dateOfBirth) {
             auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
@@ -78,6 +118,9 @@ public class RegisterActivity extends AppCompatActivity {
                         HashMap<String, String> hashMap = new HashMap<>();
                         hashMap.put("id", userId);
                         hashMap.put("username", username);
+                        hashMap.put("pesel", pesel);
+                        hashMap.put("phoneNumber", phoneNumber);
+                        hashMap.put("dateOfBirth", dateOfBirth);
                         hashMap.put("imageURL", "default");
                         hashMap.put("status", "offline");
                         hashMap.put("search", username.toLowerCase());
