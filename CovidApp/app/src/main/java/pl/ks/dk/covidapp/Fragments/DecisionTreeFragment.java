17a71a5f9b92cb5fd68981552ed73898889a4984
224323 Kasierski Dominik <224323@edu.p.lodz.pl>
2021-01-08 16:41:02 +0100

    diagnosis quesions added

--- CovidApp/app/src/main/java/pl/ks/dk/covidapp/Fragments/DecisionTreeFragment.java
+++ CovidApp/app/src/main/java/pl/ks/dk/covidapp/Fragments/DecisionTreeFragment.java
@@ -1,5 +1,7 @@
 package pl.ks.dk.covidapp.Fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,9 +11,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import pl.ks.dk.covidapp.R;
@@ -19,9 +24,9 @@ import pl.ks.dk.covidapp.R;
 public class DecisionTreeFragment extends Fragment {
 
     private List<Integer> answers;
-    private EditText answer;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
     private Button btn_submit;
-    private boolean ifListContainsNullOrEmptyStrings;
     private boolean ifListContainsNumbersFromRange;
     private final int min = 1;
     private final int max = 5;
@@ -29,32 +34,63 @@ public class DecisionTreeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //pobrac zmienna z usera, jeśli 0 to wyswietla się całe okno diagnozy, jesli 1 to wyswietla sie tylko napis "Poczekaj az lekarz sie skontaktuje"
         View view = inflater.inflate(R.layout.fragment_decision_tree, container, false);
-
         btn_submit = view.findViewById(R.id.btn_submit);
         btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Integer[] ids = {R.id.answer1, R.id.answer2, R.id.answer3, R.id.answer4, R.id.answer5, R.id.answer6, R.id.answer7, R.id.answer8, R.id.answer9, R.id.answer10};
-                answers = new ArrayList<>();
+                Integer[] ids = {R.id.radio_group1, R.id.radio_group2, R.id.radio_group3, R.id.radio_group4, R.id.radio_group5, R.id.radio_group6, R.id.radio_group7, R.id.radio_group8, R.id.radio_group9, R.id.radio_group10};
+                List<Integer> answers = new ArrayList<>();
 
                 for (Integer id : ids) {
-                    answer = (EditText) view.findViewById(id);
-                    String tmp = answer.getText().toString();
-                    if (tmp.equals("")) {
-                        Toast.makeText(getContext(), R.string.required_fields, Toast.LENGTH_SHORT).show();
-                    } else {
-                        answers.add(Integer.parseInt(tmp));
+                    radioGroup = (RadioGroup) view.findViewById(id);
+                    radioButton = view.findViewById(radioGroup.getCheckedRadioButtonId());
+                    String tmp = radioButton.getTag().toString();
+                    answers.add(Integer.parseInt(tmp));
+                }
+
+                double score = calculateWeights(answers);
+
+                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case DialogInterface.BUTTON_POSITIVE:
+                                //Ustawic zmienna jakas w userze na 1
+                                break;
+
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                //No button clicked
+                                break;
+                        }
                     }
-                }
+                };
 
-                ifListContainsNumbersFromRange = answers.stream().allMatch(i -> i >= min && i <= max);
-
-                if (!ifListContainsNumbersFromRange) {
-                    Toast.makeText(getContext(), R.string.number_range, Toast.LENGTH_SHORT).show();
-                }
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setMessage(getMessage(score)).setPositiveButton(getResources().getString(R.string.contact_with_doctor), dialogClickListener)
+                        .setNegativeButton(getResources().getString(R.string.back_to_main), dialogClickListener).show();
             }
         });
         return view;
+    }
+
+    private Double calculateWeights(List<Integer> answers) {
+        double score = 0;
+        List<Double> weights = Arrays.asList(8.8, 6.8, 3.8, 3.3, 2.0, 1.5, 1.4, 1.3, 1.1, 0.4);
+        for (int i = 0; i < answers.size(); i++) {
+            score += answers.get(i) * weights.get(i);
+        }
+        return score;
+    }
+
+    private String getMessage(double score) {
+        if (score < 2) {
+            return getResources().getString(R.string.message1);
+        } else if (score < 5) {
+            return getResources().getString(R.string.message2);
+        } else {
+            return getResources().getString(R.string.message3);
+        }
     }
 }
